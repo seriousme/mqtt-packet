5e491bc35f8b01194a422e7b318d2808d18076ae
Hans Klunder <hans.klunder@gmail.com>
2025-06-07 20:35:52 +0200

    Benchmarking Pub packet generation

---
+++ benchmarks/generateAll.js
@@ -0,0 +1,72 @@
+// This benchmark script tests 4 implementations of encoding MQTT Publish packets
+// The result is shown as Markdown table.
+
+const mqttPacket = require('../')
+const mqttPubPacket = require('./raw/pubPacket.js')
+const mqttPubPacketBuf = require('./raw/pubPacketBuffer.js')
+const mqttOpifex = require('@seriousme/opifex/mqttPacket')
+const max = 1000000
+const PUBLISH = 3
+
+const buf = Buffer.from('test')
+
+function testMQTT (mqtt) {
+  let i
+  // initialize it
+  mqtt.generate({
+    cmd: 'publish',
+    topic: 'test',
+    payload: buf
+  })
+
+  const start = Date.now()
+
+  for (i = 0; i < max; i++) {
+    mqtt.generate({
+      cmd: 'publish',
+      topic: 'test',
+      payload: buf
+    })
+  }
+
+  const time = Date.now() - start
+  return { time, packetsPerSecond: Math.floor(max / time * 1000) }
+}
+
+function testOpifex (mqtt) {
+  let i
+  // initialize it
+  mqtt.encode({
+    type: PUBLISH,
+    topic: 'test',
+    payload: buf
+  })
+
+  const start = Date.now()
+
+  for (i = 0; i < max; i++) {
+    mqtt.encode({
+      type: PUBLISH,
+      topic: 'test',
+      payload: buf
+    })
+  }
+  const time = Date.now() - start
+  return { time, packetsPerSecond: Math.floor(max / time * 1000) }
+}
+
+console.log(
+  'Round | mqtt time | mqtt packets/s | opifex time | opifex packets/s | pubPacket time | pubPacket packets/s | pubPacketBuf time | pubPacketBuf packets/s'
+)
+console.log('-- | -- | -- | -- | -- | -- | -- | -- | --')
+for (let j = 0; j < 10; j++) {
+  const mqttResult = testMQTT(mqttPacket)
+  const opifexResult = testOpifex(mqttOpifex)
+  const pubPacketResult = testMQTT(mqttPubPacket)
+  const pubPacketBufResult = testMQTT(mqttPubPacketBuf)
+  console.log(
+    `${
+      j + 1
+    } | ${mqttResult.time} | ${mqttResult.packetsPerSecond} | ${opifexResult.time} | ${opifexResult.packetsPerSecond} | ${pubPacketResult.time} | ${pubPacketResult.packetsPerSecond} | ${pubPacketBufResult.time} | ${pubPacketBufResult.packetsPerSecond}`
+  )
+}
---
+++ benchmarks/raw/pubPacketBuffer.js
@@ -0,0 +1,74 @@
+// High speed implementation of the encoding of a Publish packet
+// This version uses NodeJS Buffer.allocUnsafe to allocate the buffer and Buffer.write for strings
+
+const BitMask = {
+  bit0: 2 ** 0,
+  bit1: 2 ** 1,
+  bit2: 2 ** 2,
+  bit3: 2 ** 3,
+  bit4: 2 ** 4,
+  bit5: 2 ** 5,
+  bit6: 2 ** 6,
+  bit7: 2 ** 7
+}
+
+const PacketType = { publish: 3 }
+
+class MQTT {
+  // {
+  //     cmd: 'publish',
+  //     topic: 'test',
+  //     payload: buf
+  //   }
+
+  generate (packet) {
+    const packetType = PacketType[packet.cmd]
+    if (packetType === undefined) {
+      throw new Error(`Unknown packet type: ${packet.cmd}`)
+    }
+    // const topic = utf8Encoder.encode(packet.topic);
+    const topic = packet.topic
+    const topicLength = Buffer.byteLength(topic)
+    const payload = packet.payload
+    const payloadLength = payload.length
+    const messageId = packet.messageId || 0 // Default to 0 if not provided
+    const qos = packet.qos || 0
+    const flags = (packet.dup ? BitMask.bit3 : 0) +
+      (qos & 2 ? BitMask.bit2 : 0) +
+      (qos & 1 ? BitMask.bit1 : 0) +
+      (packet.retain ? BitMask.bit0 : 0)
+    const qosLength = qos > 0 ? 2 : 0 // QoS level requires 2 bytes if > 0
+    const remainingLength = qosLength + 2 + topicLength + payloadLength // 2 bytes for topic length
+    const result = Buffer.allocUnsafe(remainingLength + 2) // Placeholder for the result buffer
+    let offset = 0
+    // Write the fixed header
+    result[offset++] = (packetType << 4) | flags
+    // Write the remaining length
+    let remainingLengthEncoded = remainingLength
+    do {
+      let byte = remainingLengthEncoded % 128
+      remainingLengthEncoded = Math.floor(remainingLengthEncoded / 128)
+      if (remainingLengthEncoded > 0) {
+        byte |= 128 // Set the continuation bit
+      }
+      result[offset++] = byte
+    } while (remainingLengthEncoded > 0)
+    // Write the messageId if applicable
+    if (qosLength !== 0) {
+      result[offset++] = (messageId >> 8) & 0xFF // MSB
+      result[offset++] = messageId & 0xFF // LSB
+    }
+    // Write the topic length
+    result[offset++] = (topicLength >> 8) & 0xFF // MSB
+    result[offset++] = topicLength & 0xFF // LSB
+    // Write the topic
+    result.write(topic, offset)
+    offset += topicLength
+    // Write the payload
+    result.set(payload, offset)
+    offset += payloadLength
+    // Return the result as a new Uint8Array with the correct length
+    return result
+  }
+}
+module.exports = new MQTT()
